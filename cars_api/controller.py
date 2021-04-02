4046ccf4deaf988d88de746483d0fc258cf8c128
oguzhanozdmr <ademoguzhanozdmr@gmail.com>
2021-04-02 13:36:16 +0300

    update create_query function

--- cars_api/controller.py
+++ cars_api/controller.py
@@ -31,24 +31,29 @@ def args_to_query(args):
         if exterior_color in _filter['colors']:
             color_id = _filter['colors'][exterior_color]
             query += f'{to_query["exterior_color"]}={color_id}&'
-
+        else:
+            return None
     if header['brand'] in args:
         brand = args.get(header['brand']).lower()
         if brand in _filter['brands']:
             brand_id = _filter['brands'][brand]
             query += f'{to_query["brand"]}={brand_id}&'
-
+        else:
+            return None
     if header['year'] in args:
         year = args.get(header['year'])
         if year in _filter['years']:
             year_id = _filter['years'][year]
             query += f'{to_query["year"]}={year_id}&'
-
+        else:
+            return None
     if header['trans_type'] in request.args:
-        trans_type = request.args.get( header['trans_type']).lower()
+        trans_type = request.args.get(header['trans_type']).lower()
         if trans_type in _filter['trans']:
             trans_id = _filter['trans'][trans_type]
             query += f'{to_query["trans_type"]}={trans_id}&'
+        else:
+            return None
 
     query = f"?perPage={config['request_settings']['per_page']}&{query}"
     return query[:-1]
@@ -56,5 +61,7 @@ def args_to_query(args):
 
 def car_list(args):
     query = args_to_query(args)
-    get_json = GetJson(query=query)
-    return create_dict(get_json.get_requests())
+    if query:
+        get_json = GetJson(query=query)
+        return create_dict(get_json.get_requests())
+    return {'cars': []}
